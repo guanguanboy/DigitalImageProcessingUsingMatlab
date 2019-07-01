e53cce7c34a83384b5335f7fa042e869767a2a62
liguanlin <liguanlin@orbbec.com>
2019-07-01 18:00:16 +0800

    更新图像配准的实例

--- Chapter5GeometricAndImageRigstration/imageregistrationdemo.m
+++ Chapter5GeometricAndImageRigstration/imageregistrationdemo.m
@@ -1,12 +1,17 @@
-f = imread('Fig0521(a).tif');
-g = imread('Fig0521(b).tif');
-%cpselect(f, g);
+f = imread('lena.tif'); %参考图像
+g = imread('lena_rotated.tif'); %待配准图像
 subplot(2,2,1),imshow(f);
 
 subplot(2,2,2),imshow(g);
 
-%s = load('cpselect-results');
-tform = cp2tform(movingPoints, fixedPoints, 'affine');
-%figure,visreg(g, f, tform);
+% cpselect(g, f); %cpselect(moving,fixed),第一个参数是待配准图像，第二个参数是参考图像
+% save cpselect-results cpstruct %将cpstruct保存为cpselect-results.mat文件中
+s = load('cpselect-results');
+cpstruct = s.cpstruct;
+tform = cp2tform(cpstruct, 'affine');
+
+% tform = cp2tform(movingPoints2, fixedPoints2, 'affine');
+% %cp2tform的第二种调用方法
+% figure,visreg(f, g, tform); %第一个参数是参考图像，第二个参数是待配准图像
 out = imtransform(g, tform);
 subplot(2,2,3), imshow(out);
--- projective2dtest.m
+++ projective2dtest.m
@@ -4,7 +4,7 @@ close all;
 
 I = imread('lena.tif');
 %设置角度
-theta = 90;
+theta = 30;
 % 定义投影变换的矩阵
 tm = [cosd(theta) -sind(theta) 0; ...
     sind(theta) cosd(theta) 0; ...
@@ -14,6 +14,7 @@ tform = projective2d(tm);
 outputImage = imwarp(I,tform);
 figure
 imshow(outputImage);title('tform未使用默认值');
+imwrite(outputImage, 'lena_rotated.tif');
 %测试默认值的投影变换
 tform2 = projective2d;
 img2 = imwarp(I,tform2);
